b9cfc0f13a5c94e7933f48952a7c981d1eed3cd1
lazygeniuslanz <lanz.irlanda@gmail.com>
2017-11-29 16:27:00 +0800

    gallery wholesome fix

--- js/gallery/gamma.js
+++ js/gallery/gamma.js
@@ -279,7 +279,7 @@ var Gamma = (function() {
 			if( Gamma.itemsCount > 1 ) {
 
 				Gamma.svplay = $( '<button class="gamma-btn-ssplay"></button>' ).insertAfter( Gamma.svclose );
-				Gamma.nav = $( '<nav class="gamma-nav"><span class="gamma-prev"></span><span class="gamma-next"></span></nav>' ).appendTo( Gamma.singleview );
+				Gamma.nav = $( '<nav class="gamma-nav" style="background-color: #000;"><span class="gamma-prev"></span><span class="gamma-next"></span></nav>' ).appendTo( Gamma.singleview );
 				Gamma.svnavnext = Gamma.nav.find( 'span.gamma-next' );
 				Gamma.svnavprev = Gamma.nav.find( 'span.gamma-prev' );
 
